e917436ba199ae47952f3c8f0c90740d2ac45db1
kjr-code <k.rheu.code@gmail.com>
2022-11-06 11:51:00 -0500

    initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Constants {
+
+    public static int frontRight = 5;
+    public static int frontLeft = 1;
+    public static int backRight = 2; 
+    public static int backLeft = 3;
+
+    public static int actuator = 9;
+    public static int shooterSolenoidTalonL = 0;
+    public static int shooterSolenoidTalonR = 0;
+    public static int primingSolenoidL = 0;
+    public static int primingSolenoidR = 0;
+    
+}
